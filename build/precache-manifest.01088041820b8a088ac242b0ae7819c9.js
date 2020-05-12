39c62db438d9145165a79003ac15ddf60cb8b242
akshata8308 <40977061+akshata8308@users.noreply.github.com>
2020-05-12 15:44:22 +0530

    Add files via upload

---
+++ build/precache-manifest.01088041820b8a088ac242b0ae7819c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c5eef4b64464bd50708efa14686283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2854cbf02564e93b03",
+    "url": "/static/css/main.0691fb49.chunk.css"
+  },
+  {
+    "revision": "d4fdfb82f961ae6ea225",
+    "url": "/static/js/2.7219e017.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.7219e017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2854cbf02564e93b03",
+    "url": "/static/js/main.460f20e2.chunk.js"
+  },
+  {
+    "revision": "3c603e7670e7f77c9023",
+    "url": "/static/js/runtime-main.977915a5.js"
+  },
+  {
+    "revision": "b33d4fb609ae6f541e26d10b084dcf0e",
+    "url": "/static/media/image.b33d4fb6.png"
+  }
+]);
